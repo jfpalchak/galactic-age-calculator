f96c39ee0bc08bb93ed6b4d91cb0fa144ed88ed9
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 14:57:48 -0700

    Begin work on UI. Update project titles.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import { GalacticAge } from './js/galactic.js';
+
+
+
+document.querySelector("form").addEventListener("submit", () => {
+  const userAge = document.querySelector("input#user-age").value;
+
+  const galacticAge = new GalacticAge(userAge);
+  
+});
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Template',
+      title: 'Super Galactic Age Calculator',
       template: './src/index.html',
       inject: 'body'
     })
