39ffbe5e59bc971284351798e0a64c6ef251a369
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 08:41:26 -0700

    Add test 2 for GalacticAge constructor.

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -6,4 +6,16 @@ describe('GalacticAge', () => {
     let ageCalculator = new GalacticAge(25);
     expect(ageCalculator.earthYears).toEqual(25);
   });
+
+  test('should create a GalacticAge object with planet year conversion rates', () => {
+    let ageCalculator = new GalacticAge();
+    expect(ageCalculator.conversions).toEqual({
+      mercuryYear: .24,
+      venusYear: .62,
+      marsYear: 1.88,
+      jupiterYear: 11.86
+    });
+  });
+
+
 });
