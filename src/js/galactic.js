08087db97f28571f06b092246ef2745572c53d9c
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 10:37:37 -0700

    Pass yearsSince tests for Venus, Mars, and Jupiter.

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -20,9 +20,15 @@ export class GalacticAge {
   yearsSince(previousAge) {
     const earthYears = this.earthYears - previousAge;
     const mercuryYears = (earthYears / this.yearConversions["Mercury"]).toFixed(2);
+    const venusYears = (earthYears / this.yearConversions["Venus"]).toFixed(2);
+    const marsYears = (earthYears / this.yearConversions["Mars"]).toFixed(2);
+    const jupiterYears = (earthYears / this.yearConversions["Jupiter"]).toFixed(2);
     return {
       Earth: `${earthYears} Earth years have passed.`,
-      Mercury: `${mercuryYears} Mercury years have passed.`
+      Mercury: `${mercuryYears} Mercury years have passed.`,
+      Venus: `${venusYears} Venus years have passed.`,
+      Mars: `${marsYears} Mars years have passed.`,
+      Jupiter: `${jupiterYears} Jupiter years have passed.`
     }
   }
 
