f92beb3723e3f5cb29f1cb6eff6eb6092fd6bedd
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 16:21:51 -0700

    Update UI display results & simple styles.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ import './css/styles.css';
 import { GalacticAge } from './js/galactic.js';
 
 
+function displayPlanetAges(ageCalculator) {
+
+  const planets = ageCalculator.findPlanets();
+  planets.forEach((planet) => {
+    let planetAge = ageCalculator.findAgeOn(planet).toFixed(2);
+    let pTag = document.createElement('p');
+    let div = document.querySelector(`div#${planet}`);
+
+    let string = `On ${planet}, you are: ${planetAge} years old.`
+
+    div.innerText = null;
+    pTag.append(string);
+    div.append(pTag);
+  });
+
+  const ul = document.createElement('ul');
+  const liFly = document.createElement('li');
+  const liDog = document.createElement('li');
+  const divEarth = document.querySelector('div#Earth');
+  divEarth.innerText = null;
+  liFly.append(
+    `During your ${ageCalculator.findAgeOn("Earth")} years on Earth, you have outlived more than ${ageCalculator.howManyMayflies()} mayflies.`
+  );
+  liDog.append(
+    `In dog years, you are ${ageCalculator.howManyDogYears()} years old.`
+  );
+
+  divEarth.append(liFly, liDog);
+
+  
+
+}
 
 
 document.querySelector("form").addEventListener("submit", (e) => {
@@ -11,8 +43,8 @@ document.querySelector("form").addEventListener("submit", (e) => {
 
   const userAge = document.querySelector("input#user-age").value;
 
-  const galacticAge = new GalacticAge(userAge);
+  const ageCalculator = new GalacticAge(userAge);
 
-  displayResults(galacticAge);
+  displayPlanetAges(ageCalculator);
 
 });
