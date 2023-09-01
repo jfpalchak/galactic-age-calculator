55f920e85a1babf3df2991e780af8ca6a8bca51f
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 08:52:02 -0700

    Add test for inMercuryYears method.

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,13 +2,16 @@ import { GalacticAge } from './../src/js/galactic.js';
 
 describe('GalacticAge', () => {
 
+  let ageCalculator;
+  beforeEach(() => {
+    ageCalculator = new GalacticAge(25);
+  });
+
   test('should create a GalacticAge object with given age in Earth years', () => {
-    let ageCalculator = new GalacticAge(25);
     expect(ageCalculator.earthYears).toEqual(25);
   });
 
   test('should create a GalacticAge object with planet year conversion rates', () => {
-    let ageCalculator = new GalacticAge();
     expect(ageCalculator.conversions).toEqual({
       mercuryYear: .24,
       venusYear: .62,
@@ -16,6 +19,11 @@ describe('GalacticAge', () => {
       jupiterYear: 11.86
     });
   });
+
+  test('should return the given age converted to Mercury years', () => {
+    let mercuryAge = ageCalculator.inMercuryYears();
+    expect(mercuryAge).toEqual(104.17);
+  })
 
 
 });
