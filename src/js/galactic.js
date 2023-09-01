5e71a868b65fb459facd6a31fed65ff427af999e
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 09:13:01 -0700

    Refactor constructor properties.

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -4,19 +4,19 @@ export class GalacticAge {
 
   constructor(userAge) {
     this.earthYears = userAge;
-    this.conversions = {
-      mercuryYear: .24,
-      venusYear: .62,
-      marsYear: 1.88,
-      jupiterYear: 11.86
+    this.yearConversions = {
+      mercury: .24,
+      venus: .62,
+      mars: 1.88,
+      jupiter: 11.86
     };
   }
 
   inMercuryYears() {
-    return this.earthYears / this.conversions["mercuryYear"];
+    return this.earthYears / this.yearConversions["mercury"];
   }
 
   inVenusYears() {
-    return this.earthYears / this.conversions["venusYear"];
+    return this.earthYears / this.yearConversions["venus"];
   }
 }
