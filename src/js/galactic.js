f37464fc6dcd97b6a331eb656a2e8f8117e65b5d
Joey Palchak <joey.palchak@gmail.com>
2023-09-01 08:42:15 -0700

    Pass test 2 for GalacticAge constructor.

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,9 +1,17 @@
 
 
 export class GalacticAge {
+
   constructor(userAge) {
     this.earthYears = userAge;
+    this.conversions = {
+      mercuryYear: .24,
+      venusYear: .62,
+      marsYear: 1.88,
+      jupiterYear: 11.86
+    };
   }
+
 
   
 }
